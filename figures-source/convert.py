378f2f1a3c1ced5ed087edb38a7cd28fef46af34
Jean-Noel Avila <jn.avila@free.fr>
2010-09-06 21:29:45 +0200

    Extract and map all figures from graffle to svg
    Some figures are not extracted from graffle as they are screenshots.
    
    The .pot file has been obtain by running `xml2po -o figures.pot 18333fig0*.svg`
    
    The figures.pot file has been translated using poedit.
    
    The final figures can be obtained by running in fr/figures-sources for instance `xml2po -p figures.po ../../figure-sources/18333fig0101-tn.svg > figure-sources/18333fig0101-tn.svg
    
    Then the file can be converted to png by using `inkscape -z -f 18333fig0101-tn.svg -e 18333fig0101-tn.png`

---
+++ figures-source/convert.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import logging
+logging.basicConfig(level=logging.WARNING)
+
+from string import *
+import shutil
+
+for line in open('mapping.txt','r'):
+    split_line= split(line,'#')
+    values= split(split_line[0])
+    if len(values)>1:
+        logging.debug(values)
+        shutil.copyfile("progit%s.svg" % values[0],"18333fig%s-tn.svg" % values[1])
